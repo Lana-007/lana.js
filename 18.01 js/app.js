8b6259af0176cb5d7786f90e676ab0f27af573d0
Lana-007 <hovhannisyanlana5@gmail.com>
2025-01-19 17:22:21 +0400

    Add files via upload

---
+++ 18.01 js/app.js
@@ -0,0 +1,33 @@
+var a = prompt();
+if(a % 2 == 0){
+    alert("tivy zuyg e")
+}else{
+    alert("tivy kent e")
+}
+
+var b = prompt();
+if(b >= 0 && b <=9){
+    alert("mianish tiv e")
+}else if(b >= 10 && b <= 99){
+    alert("erknish tiv e")
+}else if(b >= 100 && b <= 999){
+    alert("eranish tiv e")
+}else if(b >= 1000 && b<= 9999){
+    alert("qaranish tiv e")
+}else{
+    alert("bardzr e 1 milionic")
+}
+
+var c = prompt()
+if(c > 0){
+    alert("drakan tiv e")
+}else if(c < 0){
+    alert("bacasakan tiv e")
+}
+
+var d = prompt()
+if(d % 1 == 0){
+    alert("klor tiv e")
+}else{
+    alert("tasnordakan tiv e")
+}
