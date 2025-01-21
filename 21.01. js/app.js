94f0d3ce946b03bee993a86dc4ab77585680f464
Lana-007 <hovhannisyanlana5@gmail.com>
2025-01-22 02:07:28 +0400

    Add files via upload

---
+++ 21.01. js/app.js
@@ -0,0 +1,17 @@
+var k = prompt()
+for(var i = 0; i <= k; i++){
+    console.log(i);
+}
+
+var a = prompt()
+for(var b = a; b >= 1; b--){
+    console.log(b);
+}
+
+for(var i = 1; i <= 10; i++){
+    if(i % 2 == 1){
+        document.write("<h1>hello world</h1>")
+    }else{
+        document.write("<h3>hello world</h3>")
+    }
+}
