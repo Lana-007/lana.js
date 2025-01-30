e5098ff866bf02672ec7c2f031ab05ed50d92f83
Lana-007 <hovhannisyanlana5@gmail.com>
2025-01-31 01:11:47 +0400

    Add files via upload

---
+++ 30.01. js/app.js
@@ -0,0 +1,140 @@
+var obj1 = {
+    name: "Siranush",
+    surname: "Poxosyan",
+    age: "27",
+    weight: "52kg"
+};
+
+var obj2 = {
+    name: "Vardan",
+    surname: "Sargsyan",
+    age: "44",
+    weight: "85kg"
+};
+
+var obj3 = {
+    name: "Serob",
+    surname: "Mnacakanyan",
+    age: "81",
+    weight: "95kg"
+};
+
+var obj4 = {
+    name: "Dilara",
+    surname: "Karapetyan",
+    age: "22",
+    weight: "48kg"
+};
+
+var obj5 = {
+    name: "Anahit",
+    surname: "Miqayelyan",
+    age: "39",
+    weight: "56kg"
+};
+
+var obj6 = {
+    name: "Karapet",
+    surname: "Lalayan",
+    age: "19",
+    weight: "75kg"
+};
+
+var obj7 = {
+    name: "Samvel",
+    surname: "Dovlatyan",
+    age: "52",
+    weight: "69kg"
+};
+
+var obj8 = {
+    name: "Arman",
+    surname: "Soxomonyan",
+    age: "36",
+    weight: "82kg"
+};
+
+var obj9 = {
+    name: "Hayk",
+    surname: "Smbatyan",
+    age: "47",
+    weight: "100kg"
+};
+
+var obj10 = {
+    name: "Mane",
+    surname: "Aleqsanyan",
+    age: "31",
+    weight: "62kg"
+};
+
+
+document.write("<ul>");
+document.write("<li> Name: ", obj1["name"], "</li>");
+document.write("<li> Surname: ", obj1["surname"], "</li>");
+document.write("<li> Age: ", obj1["age"], "</li>");
+document.write("<li> Weight: ", obj1["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj2["name"], "</li>");
+document.write("<li> Surname: ", obj2["surname"], "</li>");
+document.write("<li> Age: ", obj2["age"], "</li>");
+document.write("<li> Weight: ", obj2["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj3["name"], "</li>");
+document.write("<li> Surname: ", obj3["surname"], "</li>");
+document.write("<li> Age: ", obj3["age"], "</li>");
+document.write("<li> Weight: ", obj3["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj4["name"], "</li>");
+document.write("<li> Surname: ", obj4["surname"], "</li>");
+document.write("<li> Age: ", obj4["age"], "</li>");
+document.write("<li> Weight: ", obj4["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj5["name"], "</li>");
+document.write("<li> Surname: ", obj5["surname"], "</li>");
+document.write("<li> Age: ", obj5["age"], "</li>");
+document.write("<li> Weight: ", obj5["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj6["name"], "</li>");
+document.write("<li> Surname: ", obj6["surname"], "</li>");
+document.write("<li> Age: ", obj6["age"], "</li>");
+document.write("<li> Weight: ", obj6["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj7["name"], "</li>");
+document.write("<li> Surname: ", obj7["surname"], "</li>");
+document.write("<li> Age: ", obj7["age"], "</li>");
+document.write("<li> Weight: ", obj7["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj8["name"], "</li>");
+document.write("<li> Surname: ", obj8["surname"], "</li>");
+document.write("<li> Age: ", obj8["age"], "</li>");
+document.write("<li> Weight: ", obj8["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj9["name"], "</li>");
+document.write("<li> Surname: ", obj9["surname"], "</li>");
+document.write("<li> Age: ", obj9["age"], "</li>");
+document.write("<li> Weight: ", obj9["weight"], "</li>");
+document.write("</ul>");
+
+document.write("<ul>");
+document.write("<li> Name: ", obj10["name"], "</li>");
+document.write("<li> Surname: ", obj10["surname"], "</li>");
+document.write("<li> Age: ", obj10["age"], "</li>");
+document.write("<li> Weight: ", obj10["weight"], "</li>");
+document.write("</ul>");
